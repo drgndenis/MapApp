a5195c5bd1962fe6dfe843838639797f4bcf56be
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-11-01 21:54:12 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Denis DRAGAN on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
