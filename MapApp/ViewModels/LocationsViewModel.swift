1de813a002c824673548a3b56eebd0e3e072e48f
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-11-06 20:13:55 +0300

    LocationsListView added and LocationsView updated

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,53 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
+    // All loaded locations
     @Published var locations: [LocationModel]
+    // Show the list of locations
+    @Published var showLocationsList: Bool = false
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+
+    // Current location on map
+    @Published var mapLocation: LocationModel {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        // Initialize the map region
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    // Helper function to update the map region
+    private func updateMapRegion(location: LocationModel) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: span)
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: LocationModel) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
     }
 }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                Spacer()
             }
         }
     }
@@ -22,4 +28,34 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    // Header view that shows the current location and toggle button
+    private var header: some View {
+        VStack {
+            Button(action: {
+                vm.toggleLocationsList()
+            }, label: {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .foregroundStyle(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundStyle(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+            })
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(color: .black.opacity(0.3), radius: 20, y: 10)
+    }
 }
